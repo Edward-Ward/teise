1eb498ec9ab6b508b5ea62f340629799876f8593
Edward Ward <47507698+Edward-Ward@users.noreply.github.com>
2022-05-02 20:39:22 +0100

    Maps Api Introduction

--- components/Nav.js
+++ components/Nav.js
@@ -10,24 +10,27 @@ const Navbar = () => {
   };
 
   return (
-    <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-black font-medium'>
+    <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-gray-500 font-medium'>
       <h1 className='w-full text-3xl font-bold text-black'><Link href="/">Teise.</Link></h1>
       <ul className='hidden md:flex'>
-        <li className='transition ease-in-out text-xl p-6 hover:text-gray-500'><Link href="/">home</Link></li>
-        <li className='transition ease-in-out text-xl p-6 hover:text-gray-500'><Link href="/about">about</Link></li>
-        <li className='transition ease-in-out text-xl p-6 hover:text-gray-500'><Link href="/menu">menu</Link></li>
+        <li className='transition ease-in-out text-xl p-6 hover:text-black'><Link href="/">home</Link></li>
+        <li className='transition ease-in-out text-xl p-6 hover:text-black'><Link href="/about">about</Link></li>
+        <li className='transition ease-in-out text-xl p-6 hover:text-black'><Link href="/menu">menu</Link></li>
       </ul>
-      <div onClick={handleNav} className='block md:hidden'>
-          {nav ? <AiOutlineClose size={30} color="black"/> : <AiOutlineMenu size={30} color="black"/>}
+      <div onClick={handleNav} className='block md:hidden z-40'>
+          {nav ? <AiOutlineClose size={30} color="white"/> : <AiOutlineMenu size={30} color="black"/>}
+      </div> 
+      <div className={nav ? 'fixed flex flex-col text-2xl justify-center gap-60 items-center inset-0 w-[100%] h-full bg-gray-700 ease-in-out duration-500 text-white' : 'ease-in-out duration-500 fixed top-[-100%]'}>
+        <h1 className='text-3xl font-bold text-[#00df9a]'>Teise.</h1>
+        <ul className="flex flex-col gap-10 tracking-wider">
+          <li onClick={handleNav} className='font-semibold '><Link href="/">home.</Link></li>
+          <li onClick={handleNav} className='font-semibold '><Link href="/about">about.</Link></li>
+          <li onClick={handleNav} className='font-semibold '><Link href="/menu">menu.</Link></li>
+        </ul>
       </div>
-      <ul className={nav ? 'fixed left-0 top-0 w-[80%] h-full border-r border-r-gray-700 bg-gray-700 ease-in-out duration-500 text-white' : 'ease-in-out duration-500 fixed top-[-100%]'}>
-        <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4'>Teise.</h1>
-        <li onClick={handleNav} className='p-4'><Link href="/">home</Link></li>
-        <li onClick={handleNav} className='p-4'><Link href="/about">about</Link></li>
-        <li onClick={handleNav} className='p-4'><Link href="/menu">menu</Link></li>
-      </ul>
     </div>
   );
 };
 
 export default Navbar;
+
--- pages/index.js
+++ pages/index.js
@@ -3,17 +3,33 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <div className='text-black'>
-    <div className='max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center'>
-      <Typed
-        className='md:text-5xl sm:text-4xl text-xl font-bold md:pl-4 pl-2'
-          strings={['Teise', 'and', 'Coffee.']}
-          typeSpeed={120}
-          backSpeed={140}
-          loop
-        />
-      <p className='md:text-2xl text-xl font-bold text-gray-500 py-3 md:py-10'>We are an independent cafe in the heart of the Weald. <br></br>Estd. in Dec 2018.</p>
-      <Link href="/menu"><button className='transition ease-in-out bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-black hover:text-white hover:bg-[#03ae79] '>View Menu</button></Link>
+    <div className="flex text-center flex-col justify-center h-screen gap-10">
+      <div className='max-w-[800px] mt-[-96px] w-full mx-auto text-center flex flex-col justify-center'>
+        <Typed
+          className='md:text-5xl sm:text-4xl text-xl font-bold md:pl-4 pl-2'
+            strings={['Teise', 'and', 'Coffee.']}
+            typeSpeed={120}
+            backSpeed={140}
+            loop
+          />
+        <p className='md:text-2xl text-xl font-bold text-gray-500 py-3 md:py-10'>We are an <span className="text-black">independant cafe</span> in the heart of the Weald. <br></br>Estd. in Dec 2018.</p>
+        <Link href="#more"><button className='transition ease-in-out bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-black hover:text-white hover:bg-[#03ae79]'>Pop in!</button></Link>
+      </div>
+      <div className="spacer"></div><div className="spacer"></div><div className="spacer"></div><div className="spacer"></div><div className="spacer"></div>
+      <button className="animate-bounce flex self-center">
+        <Link href="#more"><svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 13l-7 7-7-7m14-8l-7 7-7-7"></path></svg></Link>
+      </button>
     </div>
+    <section className="h-screen flex items-start justify-center" id="more">
+    <a href="#" className="flex flex-col items-center bg-black rounded-lg border shadow-md md:flex-row md:max-w-xl">
+    <iframe className="p-3" width="600" height="450" loading="lazy" allowfullscreen src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJmU7_bZU530cRmdotr1yLTA8&key=AIzaSyBIhlGPoxNRyTqLtFhMQ5wjJ7BD_gMoJBA"></iframe>
+    <div className="flex flex-col justify-between p-4 leading-normal">
+        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+    </div>
+    </a>
+    </section>
   </div>
+  
   )
 }
