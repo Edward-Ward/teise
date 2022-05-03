999660b6d80b3c50ea6bfa153461a93a624e83e7
Edward Ward <47507698+Edward-Ward@users.noreply.github.com>
2022-05-03 12:02:52 +0100

    Update Map and Z index values

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
     <div className="flex text-center flex-col justify-center h-screen gap-10">
       <div className='max-w-[800px] mt-[-96px] w-full mx-auto text-center flex flex-col justify-center'>
         <Typed
-          className='md:text-5xl sm:text-4xl text-xl font-bold md:pl-4 pl-2'
+          className='md:text-5xl sm:text-4xl text-xl font-bold md:pl-4 pl-2 z-0'
             strings={['Teise', 'and', 'Coffee.']}
             typeSpeed={120}
             backSpeed={140}
@@ -16,13 +16,13 @@ export default function Home() {
         <Link href="#more"><button className='transition ease-in-out bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-black hover:text-white hover:bg-[#03ae79]'>Pop in!</button></Link>
       </div>
       <div className="spacer"></div><div className="spacer"></div><div className="spacer"></div><div className="spacer"></div><div className="spacer"></div>
-      <button className="animate-bounce flex self-center">
+      <button className="animate-bounce flex self-center z-0">
         <Link href="#more"><svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 13l-7 7-7-7m14-8l-7 7-7-7"></path></svg></Link>
       </button>
     </div>
     <section className="h-screen flex items-start justify-center" id="more">
     <a href="#" className="flex flex-col items-center bg-black rounded-lg border shadow-md md:flex-row md:max-w-xl">
-    <iframe className="p-3" width="600" height="450" loading="lazy" allowfullscreen src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJmU7_bZU530cRmdotr1yLTA8&key=AIzaSyBIhlGPoxNRyTqLtFhMQ5wjJ7BD_gMoJBA"></iframe>
+    <iframe className="p-3 min-w-[20%] max-w-200" allowfullscreen src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJmU7_bZU530cRmdotr1yLTA8&key=AIzaSyBIhlGPoxNRyTqLtFhMQ5wjJ7BD_gMoJBA"></iframe>
     <div className="flex flex-col justify-between p-4 leading-normal">
         <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
         <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
