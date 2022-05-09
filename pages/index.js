2365509d7de1ecaed5dc5b101d4913ab16bb9fcf
Edward Ward <47507698+Edward-Ward@users.noreply.github.com>
2022-05-09 09:17:14 +0100

    Blank Spacers

--- pages/index.js
+++ pages/index.js
@@ -26,7 +26,7 @@ export default function Home() {
         <h5 className="flex justify-between mb-2 text-2xl font-bold tracking-tight text-[#e0ffff]">Pop in today! <span className="text-gray-500">Or contact us <span className="pl-1 md:pl-5 text-[#00df9a]">↓</span></span></h5>
         <hr/>
         <div className="flex justify-between tracking-wide">
-          <p className="pt-8 mb-3 font-normal text-[#9db3b3] pr-2">Forge House, The Broadway, <br/>Lamberhurst, Tunbridge Wells,<br/><span className="text-[#e0ffff] text-bold tracking-wider underline decoration-inherit underline-offset-4">TN38DA</span></p>
+          <p className="pt-8 mb-3 font-normal text-[#9db3b3] pr-2">Forge House,The Broadway, <br/>Lamberhurst, Tunbridge Wells,<br/><span className="text-[#e0ffff] text-bold tracking-wider underline decoration-inherit underline-offset-4">TN38DA</span></p>
           <p className="pt-8 text-gray-500 text-bold text-right">forgehousecoffee<br className="lg:hidden"/><span className="text-[#00df9a]">@</span>gmail.com</p>
         </div>
         
