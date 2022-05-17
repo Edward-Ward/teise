4c2af92fdcf68fd798c5d8a73b132665f248685e
Edward Ward <47507698+Edward-Ward@users.noreply.github.com>
2022-05-17 18:59:59 +0100

    New ClassName

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ export default function Home() {
       </div>
       <div className="spacer hidden md:block"></div><div className="spacer hidden md:block"></div><div className="spacer"></div><div className="spacer"></div><div className="spacer"></div>
       <button className="animate-bounce flex self-center z-0">
-        <Link href="#more"><svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 13l-7 7-7-7m14-8l-7 7-7-7"></path></svg></Link>
+        <Link href="#more"><svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 13l-7 7-7-7m14-8l-7 7-7-7"></path></svg></Link>
       </button>
     </div>
     <section className="h-screen flex flex-col items-center bg-[#0D0C11]" id="more">
